ad9040d69901c4fe90a60e3474595529eeb79fbb
bahubali_ak <bahubalikurali@gmail.com>
2021-12-03 21:52:49 +0530

    initial commit added stripe integration in react with customized fields

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import {loadStripe} from '@stripe/stripe-js';
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+  CardNumberElement,
+  CardExpiryElement,
+  CardCvcElement,
+} from '@stripe/react-stripe-js';
+import { useState } from 'react';
+
+const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [postal_code, setPostalCode] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (elements == null) {
+      return;
+    }
+
+    const paymentMethod = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardNumberElement),
+      billing_details: {
+        name: 'Jenny Rosen',
+        address: {
+          postal_code: postal_code
+        }
+      },
+    }).then((response)=>{
+      console.log("token",response);
+    })
+    console.log(paymentMethod);
+    
+  };
+  
+  return (
+    <div className="text-centered">
+      <form onSubmit={handleSubmit}>
+      <CardNumberElement />
+      <CardExpiryElement />
+      <CardCvcElement />
+      <input 
+      type="text"
+      value={postal_code}
+      onChange={(e)=>{
+      setPostalCode(e.target.value)
+      console.log(e.target.value);
+      }}
+      /><br/>
+      
+      <button type="submit" disabled={!stripe || !elements}>
+        Pay
+      </button>
+    </form>
+    </div>
+    
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mt-5">
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
     </div>
   );
 }
